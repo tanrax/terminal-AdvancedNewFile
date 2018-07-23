cb21d9064d1c74c678327cb79900046e7f8433db
Andros Fenollosa <andros@fenollosa.email>
2018-07-23 22:09:55 +0200

    Update to pip

---
+++ advance_touch.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Libraries
+import os
+import click
+
+@click.command()
+@click.argument('path')
+@click.option('-cd/--change', is_flag=True, default=False, help='After creating the directories, change to the new deeper directory.')
+def advance_touch(path, cd):
+    """ Make folder and files """
+    # Make folders
+    new_dirs = '/'.join(path.split('/')[0:-1])
+    if not os.path.exists(new_dirs) and new_dirs != '':
+        os.makedirs(new_dirs)
+    # Change directory
+    if cd:
+        cd_path = os.path.join(os.getcwd(), new_dirs)
+        os.chdir(cd_path)
+
+    # Make file
+    if not path.endswith('/') and not os.path.isfile(path):
+        try:
+            open(path, 'w+').close()
+        except IsADirectoryError:
+            pass
+
+if __name__ == '__main__':
+    advance_touch()
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+setup(
+  name = 'advance-touch',
+  py_modules=['advance_touch'],
+  version = '1.0.0',
+  python_requires='>3.6',
+  description = 'Fast creation of files and directories. Mimics the operation of AdvancedNewFile (Vim plugin)',
+  author = 'Andros Fenollosa',
+  author_email = 'andros@fenollosa.email',
+  url = 'https://github.com/tanrax/terminal-AdvancedNewFile',
+  keywords = ['touch', 'mkdir', 'advance', 'vim'],
+  classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+  ),
+  install_requires=[
+      'Click>=6.7',
+  ],
+  entry_points='''
+      [console_scripts]
+      ad=advance_touch:advance_touch
+  '''
+)
